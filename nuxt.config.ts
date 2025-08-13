ad56699462ecd848247450204c76282d9f8d2c02
Yang Yang <persistence@vip.qq.com>
2025-08-13 15:12:21 +0800

    updated nuxt to v4 and installed shadcn vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   devServer: {
     port: 3001,
     host: '0.0.0.0',
   },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   css: [
     '~/assets/css/main.css'
   ],
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', 'shadcn-nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
   devtools: { enabled: true },
   routeRules: {
     // prerender index route by default
@@ -16,5 +34,4 @@ export default defineNuxtConfig({
   "ui": {
     "fonts": false
   }
-
 });
